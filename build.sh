3f5d5db7b4388f308b3f9169106c0581c48a4720
Zdenek Dolezal <Griperis@outlook.cz>
2022-11-23 10:28:27 +0100

    Fix replacing version in build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="1.0.0"
+VERSION="1.0.1"
 BUILDS_FOLDER="builds"
 
 rm -rf ${BUILDS_FOLDER}/attribute_viewer
@@ -11,7 +11,7 @@ cp __init__.py ${BUILDS_FOLDER}/attribute_viewer
 cp data/attribute_viewer_nodes.blend ${BUILDS_FOLDER}/attribute_viewer/data
 
 # change version in bl_info to match one in this file
-sed -i "s/'version': ([0-9], [0-9], [0-9])/'version': (`echo ${VERSION} | sed -e 's/\./, /g'`)/" ${BUILDS_FOLDER}/attribute_viewer/__init__.py
+sed -i "s/\"version\": ([0-9], [0-9], [0-9])/\"version\": (`echo ${VERSION} | sed -e 's/\./, /g'`)/" ${BUILDS_FOLDER}/attribute_viewer/__init__.py
 
 
 # remove old zip, zip everything
